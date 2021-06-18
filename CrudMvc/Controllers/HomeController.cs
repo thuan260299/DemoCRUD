3d3583452b4d957b07641baec8a0d1e646651e2a
THUẬN ĐỖ <thuan26299@gmail.com>
2021-06-18 14:41:30 +0700

    first commit

---
+++ CrudMvc/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrudMvc.Models;
+
+namespace CrudMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+//        public ActionResult PagingSortingAndSearching(string sortOn, string orderBy,
+//string pSortOn, string keyword, int? page)
+//        {
+//            using (CRUBEntities db=new CRUBEntities())
+            
+//            if (!page.HasValue)
+//            {
+//                page = 1; // set initial page value
+//                if (string.IsNullOrWhiteSpace(orderBy) || orderBy.Equals("asc"))
+//                {
+//                    orderBy = "desc";
+//                }
+//                else
+//                {
+//                    orderBy = "asc";
+//                }
+//            }
+
+//            // override the sort order if the previous sort order and current request sort order is different
+//            if (!string.IsNullOrWhiteSpace(sortOn) && !sortOn.Equals(pSortOn,StringComparison.CurrentCultureIgnoreCase))
+//            {
+//                orderBy = "asc";
+//            }
+
+//            ViewBag.OrderBy = orderBy;
+//            ViewBag.SortOn = sortOn;
+//            ViewBag.Keyword = keyword;
+
+//            var list = db.PersonalDetails.AsQueryable();
+
+//            switch (sortOn)
+//            {
+//                case "FirstName":
+//                    if (orderBy.Equals("desc"))
+//                    {
+//                        list = list.OrderByDescending(p => p.FirstName);
+//                    }
+//                    else
+//                    {
+//                        list = list.OrderBy(p => p.FirstName);
+//                    }
+//                    break;
+//                case "LastName":
+//                    if (orderBy.Equals("desc"))
+//                    {
+//                        list = list.OrderByDescending(p => p.LastName);
+//                    }
+//                    else
+//                    {
+//                        list = list.OrderBy(p => p.LastName);
+//                    }
+//                    break;
+//                case "Age":
+//                    if (orderBy.Equals("desc"))
+//                    {
+//                        list = list.OrderByDescending(p => p.Age);
+//                    }
+//                    else
+//                    {
+//                        list = list.OrderBy(p => p.Age);
+//                    }
+//                    break;
+//                default:
+//                    list = list.OrderBy(p => p.AutoId);
+//                    break;
+//            }
+//            if (!string.IsNullOrWhiteSpace(keyword))
+//            {
+//                list = list.Where(f => f.FirstName.StartsWith(keyword));
+//            }
+          
+//            return View(finalList);
+//        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CrudMvc/Controllers/UserController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrudMvc.Models;
+
+namespace CrudMvc.Controllers
+{
+    public class UserController : Controller
+    {
+        CRUBEntities db = new CRUBEntities();
+        // GET: User
+        //public ActionResult Index()
+        //{
+        //    using (CRUBEntities db = new CRUBEntities())
+        //    {
+        //        return View(db.User.ToList());
+        //    }
+
+        //}
+
+        public ActionResult Index(string searchString)
+        {
+            using (CRUBEntities db = new CRUBEntities())
+            {
+                
+                    //use= (User)db.User.Where(s => s.Ten.StartsWith(searchString));
+                    return View(db.User.Where(x => x.Ten.Contains(searchString) || searchString == null).ToList());
+            
+            }
+
+         
+        }
+
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            using (CRUBEntities db= new CRUBEntities())
+            {
+                return View(db.User.Where(x=>x.Id==id).FirstOrDefault());
+            }
+            
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(User use)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (CRUBEntities db = new CRUBEntities())
+                {
+                    db.User.Add(use);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (CRUBEntities db = new CRUBEntities())
+            {
+                return View(db.User.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, User use)
+        {
+            try
+            {
+                   // TODO: Add update logic here
+                using(CRUBEntities db= new CRUBEntities())
+                {
+                    db.Entry(use).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (CRUBEntities db = new CRUBEntities())
+            {
+                return View(db.User.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, User use)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (CRUBEntities db = new CRUBEntities())
+                {
+                    use = db.User.Where(x => x.Id == id).FirstOrDefault();
+                    db.User.Remove(use);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
